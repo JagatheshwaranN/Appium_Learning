f0404b698162480b355aadd4b4a5c00d6deee23f
Jagatheshwaran N <jagatheshwaran.n29@gmail.com>
2026-07-10 05:39:10 -0500

    App Interaction Test Added & Code Refactored

--- src/test/java/com/jaga/AppiumBasicsTest.java
+++ src/test/java/com/jaga/AppiumBasicsTest.java
@@ -2,24 +2,16 @@ package com.jaga;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
-import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
 import org.testng.annotations.Test;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-public class AppiumBasicsTest {
+public class AppiumBasicsTest extends BaseTest {
 
     @Test
     public void appiumTest() throws URISyntaxException, MalformedURLException, InterruptedException {
-
-        //Server Start
-        AppiumDriverLocalService service = new AppiumServiceBuilder().withAppiumJS(new File("C://Users//jagat//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
-                .withIPAddress("127.0.0.1").usingPort(4723).build();
-        service.start();
 
         UiAutomator2Options options = new UiAutomator2Options();
         options.setDeviceName("Jaga Phone");
@@ -28,7 +20,5 @@ public class AppiumBasicsTest {
         Thread.sleep(3000);
         driver.quit();
 
-        // Server Stop
-        service.stop();
     }
 }
---
+++ src/test/java/com/jaga/BaseTest.java
@@ -0,0 +1,27 @@
+package com.jaga;
+
+import io.appium.java_client.service.local.AppiumDriverLocalService;
+import io.appium.java_client.service.local.AppiumServiceBuilder;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.io.File;
+
+public class BaseTest {
+
+    private AppiumDriverLocalService service;
+
+    @BeforeTest
+    public void startServer() {
+        //Server Start
+        service = new AppiumServiceBuilder().withAppiumJS(new File("C://Users//jagat//AppData//Roaming//npm//node_modules//appium//build//lib//main.js"))
+                .withIPAddress("127.0.0.1").usingPort(4723).build();
+        service.start();
+    }
+
+    @AfterTest
+    public void stopServer() {
+        // Server Stop
+        service.stop();
+    }
+}
