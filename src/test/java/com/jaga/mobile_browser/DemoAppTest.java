00db78abf73bb0a99f238dd2f97d59d4920ca809
Jagatheshwaran N <jagatheshwaran.n29@gmail.com>
2026-07-16 05:06:03 -0500

    Mobile Browser Test Scenarios Added

---
+++ src/test/java/com/jaga/mobile_browser/DemoAppTest.java
@@ -0,0 +1,31 @@
+package com.jaga.mobile_browser;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoAppTest extends MobileBrowserTest {
+
+    @Test
+    public void launchChromeTest() throws InterruptedException {
+        driver.get("https://www.google.com/");
+        System.out.println(driver.getTitle());
+        driver.findElement(By.name("q")).sendKeys("Appium");
+        driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
+    }
+
+    @Test
+    public void demoAppTest() throws InterruptedException {
+        driver.get("https://rahulshettyacademy.com/angularAppdemo/");
+        System.out.println(driver.getTitle());
+        driver.findElement(By.xpath("//span[@class='navbar-toggler-icon']")).click();
+        driver.findElement(By.cssSelector("a[routerlink*='products']")).click();
+        driver.executeScript("window.scrollBy(0, 1000)", "");
+        String text = driver.findElement(By.cssSelector("a[href*='products/3']")).getText();
+        Assert.assertEquals(text, "Devops");
+        Thread.sleep(3000);
+    }
+
+}
